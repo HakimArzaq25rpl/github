ad1f268314bc9b805ff1ec0c8cc361a315a7cd3b
Hakim <mohammad_arzaq_25rpl@student.smktelkom-mlg.sch.id>
2018-01-11 10:43:37 +0700

    first commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/mohammad_arzaq_25rplstudent/hellogit/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.mohammad_arzaq_25rplstudent.hellogit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
